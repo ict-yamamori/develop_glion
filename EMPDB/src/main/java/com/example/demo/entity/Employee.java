7098eca2976b136ce4d44de78bb674c5c1090b80
GL012100 <GL012100@GL012100-PC>
2022-11-02 11:47:07 +0900

    兼務情報登録の際のバグ修正

--- EMPDB/src/main/java/com/example/demo/entity/Employee.java
+++ EMPDB/src/main/java/com/example/demo/entity/Employee.java
@@ -10,30 +10,26 @@ import lombok.Data;
 @Data
 public class Employee {
 	private int id;
-	@NotBlank(message = "※省略することはできません")
+	@NotBlank(message = "※名前を省略することはできません")
 	private String name;
-	@NotBlank(message = "※省略することはできません")
+	@NotBlank(message = "※フリガナを省略することはできません")
 	private String name_kana;
-	//@NotBlank(message = "省略することはできません")
 	private String status;
 	private String telephonenumber;
 	private String mailaddress;
-	@NotNull(message = "※省略することはできません")
+	@NotNull(message = "※グループ入社日を省略することはできません")
 	private Date entering_date;
 	private Date leaving_date;
 	private String business_org_name;
 	private String division_name;
-	//@NotBlank(message = "省略することはできません")
 	private String company_name;
 	private String gen_bra_name;
 	private String branch_name;
 	private String department;
-	//@NotBlank(message = "省略することはできません")
 	private String emp_job_name;
 	private String official_position;
-	//@NotBlank(message = "省略することはできません")
 	private String employment_type;
-	@NotNull(message = "※省略することはできません")
+	@NotNull(message = "※所属開始日を省略することはできません")
 	private Date start_date;
 	private Date end_date;
 	private String second_company_name;
@@ -41,7 +37,6 @@ public class Employee {
 	private Integer employee_id;
 	private Integer business_org_id;
 	private Integer division_id;
-	@NotNull(message = "※省略することはできません")
 	private Integer company_id;
 	private Integer gen_bra_id;
 	private Integer branch_id;
